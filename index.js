001a966d2dac6ab20b71797c0e0b88968bf9c7dc
Fuad <sintfd@gmail.com>
2025-02-04 23:31:23 +0400

    all task finish except task_9

--- index.js
+++ index.js
@@ -12,23 +12,23 @@ document.getElementById("task_1").addEventListener("click", function () {
 // в коде как константу
 
 document.getElementById("task_2").addEventListener("click", function () {
-  const age = 2025 - Number(prompt("Живо год рождения сюды:"));
+  const year = 2025 - Number(prompt("Год рождения сюды:"));
   let yearsOld = null;
 
-  if (age <= 10) {
+  if (year <= 10) {
     yearsOld = "очень молодой";
-  } else if (age <= 30) {
+  } else if (year <= 30) {
     yearsOld = "юнец";
-  } else if (age <= 40) {
+  } else if (year <= 40) {
     yearsOld = "почти старый";
-  } else if (age <= 60) {
+  } else if (year <= 60) {
     yearsOld = "уже старый";
-  } else if (age <= 110) {
+  } else if (year <= 110) {
     yearsOld = "очень старый";
-  } else if (age >= 110) {
+  } else if (year >= 110) {
     yearsOld = " ещё жив ???";
   }
-  alert(`Тебе: ${age}  ,  Ты ${yearsOld}`);
+  alert(`Тебе: ${year}  ,  Ты ${yearsOld}`);
 });
 
 // --------------------------------------------------------------------------------------------------------------------------
@@ -48,4 +48,98 @@ document.getElementById("task_3").addEventListener("click", function () {
 document.getElementById("task_4").addEventListener("click", function () {
   const radius = Math.PI * Number(prompt("Радиус круга:")) ** 2;
   alert(`Периметр квадрата: ${radius} см`);
+});
+
+// --------------------------------------------------------------------------------------------------------------------------
+
+// 5. Запросите у пользователя расстояние в км между двумя
+// городами и за сколько часов он хочет добраться. Посчитайте скорость, с которой необходимо двигаться, чтобы
+// успеть вовремя.
+
+document.getElementById("task_5").addEventListener("click", function () {
+  const distance = Number(prompt("Расстояние (в км):"));
+  const time = Number(prompt("За сколько часов:"));
+  let speed = distance / time;
+
+  if (speed > 880) {
+    alert("Вам нужен самолёт !!!");
+  } else {
+    alert(`Двигайтесь со скоростью: ${speed} км/ч`);
+  }
+});
+
+// --------------------------------------------------------------------------------------------------------------------------
+
+// 6. Реализуйте конвертор валют. Пользователь вводит доллары, программа переводит в евро. Курс валюты храните в
+// константе.
+
+document.getElementById("task_6").addEventListener("click", function () {
+  const usdToEur = 0.9596;
+  const usd = Number(prompt("Введите сумму (в USD)"));
+  alert(`${usdToEur * usd} EUR`);
+});
+
+// --------------------------------------------------------------------------------------------------------------------------
+
+// 7. Пользователь указывает объем флешки в Гб. Программа
+// должна посчитать сколько файлов размером в 820 Мб помещается на флешку
+
+document.getElementById("task_7").addEventListener("click", function () {
+  const flashСapacity = 1024 * Number(prompt("Введите объем флешки (в Гб)"));
+
+  alert(
+    `На флешку вместится ${Math.trunc(
+      flashСapacity / 820
+    )} файлов размером в 820 Мб`
+  );
+});
+
+// 8. Пользователь вводит сумму денег в кошельке и цену одной
+// шоколадки. Программа выводит сколько шоколадок может
+// купить пользователь и сколько сдачи у него останется.
+
+document.getElementById("task_8").addEventListener("click", function () {
+  const money = Number(prompt("Сколько у вас денег ???"));
+  const price = Number(prompt("Цена одной шоколадки ???"));
+  const amount = Math.trunc(money / price);
+
+  alert(
+    `Вы можете купить: ${amount} шоколадки и у вас останется ${
+      money - price * amount
+    } AZN`
+  );
+});
+
+// --------------------------------------------------------------------------------------------------------------------------
+
+// 9. Запросите у пользователя трехзначное число и выведите
+// его задом наперед. Для решения задачи вам понадобится
+// оператор % (остаток от деления).
+
+document.getElementById("task_9").addEventListener("click", function () {
+  let i = 0;
+  const number = prompt("Введите число");
+  let turnOver = null;
+
+  while (i < number.length) {
+    i++;
+    turnOver = String(turnOver + Number(number[number.length - i]));
+  }
+
+  alert(turnOver);
+
+  // alert(`${number[2] + number[1] + number[0]}`);
+});
+
+// --------------------------------------------------------------------------------------------------------------------------
+
+// 10. Запросите у пользователя целое число и выведите в ответ,
+// четное число или нет. В задании используйте логические
+// операторы. В задании не надо использовать if или switch.
+
+document.getElementById("task_10").addEventListener("click", function () {
+  const number = Number(Math.trunc(prompt("Введите число")));
+
+  Number(number % 2) === 0 && alert(`Чётный`);
+  Number(number % 2) !== 0 && alert(`Нечётный !!!`);
 });
